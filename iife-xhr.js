189c4533a7be6c33cbc4fcd04de48d5d9df78cf7
Scott Humphries <git@sscotth.io>
2016-07-27 10:26:08 -0500

    add data and fix callbacks

--- iife-xhr.js
+++ iife-xhr.js
@@ -2,7 +2,7 @@ function showCarnivores (carnivores) {
   var list = document.querySelector('.carnivores')
 
   carnivores.forEach(function (carnivore) {
-    list.innerHTML += `<li>carnivore</li>`
+    list.innerHTML += `<li>${carnivore}</li>`
   })
 }
 
@@ -10,7 +10,7 @@ function showHerbivores (herbivores) {
   var list = document.querySelector('.herbivores')
 
   herbivores.forEach(function (herbivore) {
-    list.innerHTML += `<li>herbivore</li>`
+    list.innerHTML += `<li>${herbivore}</li>`
   })
 }
 
