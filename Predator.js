85eee7639eea5db339676d5a1a209d41b3ae2672
Scott Humphries <git@sscotth.io>
2016-07-27 10:09:48 -0500

    finished step 4

--- Predator.js
+++ Predator.js
@@ -1,4 +1,25 @@
 var Predator = (function () {
   var carnivores = []
   var herbivores = []
+
+  return {
+    getCarnivores: function (cb) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'carnivores.json')
+      xhr.addEventListener('load', function (evt) {
+        carnivores = JSON.parse(evt.target.responseText)
+        cb(carnivores)
+      })
+      xhr.send()
+    },
+    getHerbivores: function (cb) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'herbivores.json')
+      xhr.addEventListener('load', function (evt) {
+        herbivores = JSON.parse(evt.target.responseText)
+        cb(herbivores)
+      })
+      xhr.send()
+    }
+  }
 }())
