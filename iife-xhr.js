e9cf4ffb60eeef8ff0e78f6a599b8137c44eb04b
Scott Humphries <git@sscotth.io>
2016-07-27 10:21:08 -0500

    finished step 5

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+function showCarnivores (carnivores) {
+  var list = document.querySelector('.carnivores')
+
+  carnivores.forEach(function (carnivore) {
+    list.innerHTML += `<li>carnivore</li>`
+  })
+}
+
+function showHerbivores (herbivores) {
+  var list = document.querySelector('.herbivores')
+
+  herbivores.forEach(function (herbivore) {
+    list.innerHTML += `<li>herbivore</li>`
+  })
+}
+
+Predator.getCarnivores(showCarnivores);
+Predator.getHerbivores(showHerbivores);
